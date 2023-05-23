f26156f8ee58c3102357b6a7b043c6c0bef6d9e9
9wynneth <95265271+9wynneth@users.noreply.github.com>
2023-05-23 22:32:22 +0700

    html and js done

--- data.php
+++ data.php
@@ -1,22 +1,53 @@
 <?php
 include 'connection.php';
 
+// Retrieve the query from the AJAX request
+$query = $_POST['query'];
 
-// Query to retrieve data from the database
-$sql = "SELECT * FROM `customer`";
-$result = $conn->query($sql);
+// Execute the query
+$result = $conn->query($query);
+
+// Fetch the column names
+$columns = array();
+if ($result->num_rows > 0) {
+  $row = $result->fetch_assoc();
+  $columns = array_keys($row);
+}
 
 // Fetch the data and store it in an array
 $data = array();
 if ($result->num_rows > 0) {
-    while ($row = $result->fetch_assoc()) {
-        $data[] = $row;
-    }
+  do {
+    $data[] = $row;
+  } while ($row = $result->fetch_assoc());
 }
 
 // Close the connection
 $conn->close();
 
-// Send the data as JSON
-echo json_encode($data);
+// Generate the HTML code for the table
+$html = '
+  <table id="example2" class="display dataTable no-footer" style="width: 100%" aria-describedby="example_info">
+    <thead>
+      <tr>';
+foreach ($columns as $column) {
+  $html .= '<th>' . $column . '</th>';
+}
+$html .= '
+      </tr>
+    </thead>
+    <tbody>';
+foreach ($data as $row) {
+  $html .= '<tr>';
+  foreach ($row as $value) {
+    $html .= '<td>' . $value . '</td>';
+  }
+  $html .= '</tr>';
+}
+$html .= '
+    </tbody>
+  </table>';
+
+// Return the HTML code
+echo $html;
 ?>
